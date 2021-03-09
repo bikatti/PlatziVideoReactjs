5cd7222e72be0db7e33be4dbfa4a819b2731835f
SebasTyler <ibikatti@gmail.com>
2021-03-09 10:23:34 -0400

    presetting done

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import '../assets/styles/App.scss'
 
-const HelloWorld = () => {
-    return (
-        <div>
-            <h1>Hola mundi</h1>
-        </div>
-    )
-}
+const HelloWorld = () => (
+    <div>
+        <h1>Hola mundo</h1>
+    </div>
+)
+
 
 export default HelloWorld
